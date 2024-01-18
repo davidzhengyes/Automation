330d5c6ba6cff136772bb335977b72884956e177
David Zheng <81645746+davidzhengyes@users.noreply.github.com>
2024-01-18 00:59:34 -0500

    movin some stuff grindin

--- cleanupcopy.py
+++ cleanupcopy.py
@@ -1,54 +1,24 @@
 # selenium 4
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from discord_webhook import DiscordWebhook
-import undetected_chromedriver as uc
+import json
 import os 
 import time
 import ast
 from seleniumbase import SB
 
-# options = uc.ChromeOptions() 
-# options.headless = False
-# driver = uc.Chrome(options=options) 
-
-
-# def launchBrowser():
-#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-#     driver.get("https://lzt.market/steam/rust?hours_played[252490]=800&country[]=Canada&country[]=United%20States&mm_ban=nomatter&order_by=price_to_up")
-#     return driver
-
-# driver=launchBrowser()
-
-# with driver:
-#     # Go to the target website
-#     driver.get("https://lzt.market/steam/rust?hours_played[252490]=800&country[]=Canada&country[]=United%20States&mm_ban=nomatter&order_by=price_to_up")
-# # Wait for security check
-
 
     
-print("DRIVER RECONNECTED!")
+file = open('lztparams.json')
+data=json.load(file)
 
-
-# notRobot = sb.find_element(By.CSS_SELECTOR,"body > div > form > button")
-# notRobot.click()
-
-# ruski = driver.find_element(By.CSS_SELECTOR,"#content > div > div > div.mainContainer > div > div.FloatingContainer.Notices > div > div > div > a")
-# ruski.click()
-# time.sleep(4)
-# time.sleep(3)
-# import atexit
-# #Accts = driver.find_elements(By.CLASS_NAME,'marketIndexItem--otherInfo')
-# def exit_handler():
-#     file=open("visited.txt","w")
-#     file.write(str(visited))
-#     file.close()  
-# atexit.register(exit_handler)
-# #acc prob not needed as write to fiel on regular basis
+refreshSeconds = data["frequency_seconds"]
+discordTag = data["userID"]
+webhookLink = data["DISCORD_WEBHOOK_URL"]
+lztlink = data["lzt_page"]
 
 if os.path.getsize('visited.txt') == 0:
     visited=set()
@@ -65,8 +35,8 @@ def scanNewAccts():
     time.sleep(2)
     Accts = sb.find_elements(By.CLASS_NAME,'marketIndexItem.PopupItemLink.PopupItemLinkActive')
     print(Accts)
-    print(Accts[0])
-    print("a")
+  
+
     for acct in Accts:
         
         #would be faster to use XPATH (XML) selector
@@ -88,10 +58,11 @@ def scanNewAccts():
 
         if acctID in visited:
             pass
+            print("AAAAAAAAAA SAW THIS ACC ALREADY")
         else:
             visited.add(acctID)
-            msg="New account posted by"+ seller + timesinceposting + acctID + " @SATCHELPOOCH"
-            webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/1197037155554443314/uxp9jMz5aPyhQaA0lcdq1MSXCEC-5JupagoCU6H5ufAwXNES4no0K2w_tryoYiKUBKqC', content=msg)
+            msg="New account posted by"+ seller + timesinceposting + acctID + discordTag
+            webhook = DiscordWebhook(url=webhookLink, content=msg)
             response = webhook.execute()
         file=open("visited.txt","w")
         file.write(str(visited))
@@ -100,7 +71,7 @@ def scanNewAccts():
     
 with SB(uc=True) as sb:
     sb.driver.uc_open_with_reconnect(
-        "https://lzt.market/steam/rust?hours_played[252490]=800&country[]=Canada&country[]=United%20States&mm_ban=nomatter&order_by=price_to_up",
+        lztlink ,
         reconnect_time=30
     )
 
@@ -112,7 +83,7 @@ with SB(uc=True) as sb:
         print("ITERATION \n")
         print(visited)
 
-        time.sleep(30)
+        time.sleep(int(refreshSeconds))
 
 print("b")
 
---
+++ fullcleancopy.py
@@ -0,0 +1,89 @@
+# selenium 4
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from discord_webhook import DiscordWebhook
+import json
+import os 
+import time
+import ast
+from seleniumbase import SB
+
+
+    
+file = open('lztparams.json')
+data=json.load(file)
+
+refreshSeconds = data["frequency_seconds"]
+discordTag = data["userID"]
+webhookLink = data["DISCORD_WEBHOOK_URL"]
+lztlink = data["lzt_page"]
+
+if os.path.getsize('visited.txt') == 0:
+    visited=set()
+else:
+    file=open("visited.txt","r")
+    visited=ast.literal_eval(file.read())
+    file.close()
+
+
+def scanNewAccts():
+    WebDriverWait(sb, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#title > div > span"))).click()
+    resetbtn = sb.driver.find_element(By.CSS_SELECTOR,"#title > div > span")
+    resetbtn.click()
+    time.sleep(2)
+    Accts = sb.find_elements(By.CLASS_NAME,'marketIndexItem.PopupItemLink.PopupItemLinkActive')
+    #print(Accts)
+  
+
+    for acct in Accts:
+        
+        #would be faster to use XPATH (XML) selector
+        #not learning allat for now, no time
+    
+        topcontainer = acct.find_element(By.CLASS_NAME,"marketIndexItem--topContainer")
+        insideh4 = topcontainer.find_element(By.TAG_NAME,"h4")
+        atag = insideh4.find_element(By.CLASS_NAME,"marketIndexItem--Title.PopupItemLink")
+        acctID = atag.get_attribute("href")
+        #can also take acctID from Accts as a comparison base, maybe XPATH not faster
+        #bc kinda slow #haha my laptop is just slow nvm it's fine
+       
+
+    
+        inside = acct.find_element(By.CLASS_NAME,"marketIndexItem--otherInfo")
+        seller = inside.text.split(" ")[0]
+        timesinceposting = inside.text.split(" ")[1] + ' ' + inside.text.split(" ")[2]
+        # print(inside.text.split(" ")[0],inside.text.split(" ")[1],inside.text.split(" ")[2],acctID)
+
+        if acctID in visited:
+            pass
+            # print("AAAAAAAAAA SAW THIS ACC ALREADY")
+        else:
+            visited.add(acctID)
+            msg="New account posted by"+ seller + timesinceposting + acctID + discordTag
+            webhook = DiscordWebhook(url=webhookLink, content=msg)
+            response = webhook.execute()
+        file=open("visited.txt","w")
+        file.write(str(visited))
+        file.close()
+        #then overwrite everything in visited set into visited.txt 
+    
+with SB(uc=True) as sb:
+    sb.driver.uc_open_with_reconnect(
+        lztlink ,
+        reconnect_time=30
+    )
+
+    #print("DRIVER RECON")
+    while True:
+    
+        scanNewAccts()
+        
+        # print("ITERATION \n")
+        # print(visited)
+
+        time.sleep(int(refreshSeconds))
+
+print("b")
+
